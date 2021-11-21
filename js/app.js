763d8dfde7e54469bf1ef2df85900288e40a2311
ttyngai <ttyngai@gmail.com>
2021-11-20 16:35:20 -0800

    Add app function framework

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+init();
+
+function init() {
+  render();
+}
+
+function render() {}
+
+function dealPlayer() {
+  render();
+}
+
+// Helper Function
+function randomCard() {}
